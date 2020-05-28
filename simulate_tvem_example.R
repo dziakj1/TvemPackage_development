cbdabab1d82827e590c0ad62c0eea7a08e2b7db7
John J. Dziak <jjd264@psu.edu>
2020-05-28 16:30:11 -0400

    Moved more data-generating parameters to options in simulate_tvem_example.R

--- simulate_tvem_example.R
+++ simulate_tvem_example.R
@@ -6,25 +6,62 @@
 #' 
 #' @param n_subjects Number of subjects in dataset 
 #' @param max_time The time point at the end of the simulated time interval
-#' 
+#' @param n_subjects  
+#' @param max_time 
+#' @param simulate_binary 
+#' @param n_obs_possible  
+#' @param prop_obs_observed  
+#' @param sigma_x1  
+#' @param sigma_x2  
+#' @param truncate_for_realism 
+#' @param round_digits 
+#' @param x1_short_term_rho 
+#' @param x2_short_term_rho 
+#' @param sigma_y  
+#' @param mu_x1_function 
+#' @param mu_x2_function  
+#' @param beta0_y_function 
+#' @param beta1_y_function  
+#' @param beta2_y_function  
+#' @param y_short_term_rho  
+#' @return A simulated dataset with the following variables:
+#' \describe{
+#' \item{subject_id}{Subject ID}
+#' \item{time}{Observation time}
+#' \item{x1}{First covariate}
+#' \item{x2}{Second covariate}
+#' \item{y}{Outcome variable}
+#' }   
 #' @export
 
 simulate_tvem_example <- function(
   n_subjects = 300, 
   max_time = 7,
-  simulate_binary = FALSE) {
-  n_obs_possible <- 141;
-  prop_obs_observed <- .3; 
-  sigma_x1 <- 2;
-  sigma_x2 <- 2;
-  truncate_for_realism <- TRUE;
+  simulate_binary = FALSE,
+  n_obs_possible = 141,
+  prop_obs_observed = .3, 
+  sigma_x1 = 2,
+  sigma_x2 = 2,
+  truncate_for_realism = TRUE,
+  round_digits = 3,
+  x1_short_term_rho = .7,
+  x2_short_term_rho = .7,
+  sigma_y = 1.5,
+  mu_x1_function = function(t) {6 - 2*sqrt(pmax(0,(t/7)-.2))},
+  mu_x2_function = function(t) {3 + sqrt(pmax(0,(t/7)-.5))},
+  beta0_y_function = function(t) {2 - .3*sqrt(t/7)},
+  beta1_y_function = function(t) {.5*(t/7)^2}, 
+  beta2_y_function = function(t) {rep(.2,length(t))},
+  y_short_term_rho = 0.8 
+  ) {
   possible_observation_times <- seq(0,max_time,length=n_obs_possible);
-  scaled_times <- possible_observation_times/max_time;
-  print(summary(scaled_times));
+  mu_x1 <- mu_x1_function(possible_observation_times);
+  mu_x2 <- mu_x2_function(possible_observation_times); 
+  beta0_y <- beta0_y_function(possible_observation_times);
+  beta1_y <- beta1_y_function(possible_observation_times);
+  beta2_y <- beta2_y_function(possible_observation_times);
   n_obs_per_subject <- rbinom(n_subjects,n_obs_possible,prop_obs_observed);
     # Generate X1;
-  mu_x1 <- 6 - 2*sqrt(pmax(0,scaled_times-.2));
-  x1_short_term_rho <- .7;
   x1_error_term_AR1 <- matrix(NA, n_subjects, n_obs_possible);
   x1_error_term_AR1[,1] <- rnorm(n_subjects);
   for (j in 2:n_obs_possible) {
@@ -32,10 +69,8 @@ simulate_tvem_example <- function(
       sqrt(1-x1_short_term_rho^2)*rnorm(n_subjects);
   }
   x1 <- t(apply(sigma_x1*x1_error_term_AR1,1,"+",mu_x1));
-  x1 <- round(x1,3);
+  x1 <- round(x1,round_digits);
   # Generate X2;
-  mu_x2 <- 3 + sqrt(pmax(0,scaled_times-.5));
-  x2_short_term_rho <- .7;
   x2_error_term_AR1 <- matrix(NA, n_subjects, n_obs_possible);
   x2_error_term_AR1[,1] <- rnorm(n_subjects);
   for (j in 2:n_obs_possible) {
@@ -43,20 +78,14 @@ simulate_tvem_example <- function(
       sqrt(1-x2_short_term_rho^2)*rnorm(n_subjects);
   }
   x2 <- t(apply(sigma_x2*x2_error_term_AR1,1,"+",mu_x2));
-  x2 <- round(x2,3);
+  x2 <- round(x2,round_digits);
   # Generate Y;
-  sigma_y <- 1.5;
-  beta0_y <- 2 - .3*sqrt(scaled_times);
-  beta1_y <- .5*scaled_times^2; 
-  beta2_y <- rep(.2,length(scaled_times));
   matrix_beta0_y <- matrix(rep(beta0_y,n_subjects),byrow=TRUE,nrow=n_subjects);
   matrix_beta1_y <- matrix(rep(beta1_y,n_subjects),byrow=TRUE,nrow=n_subjects);
   matrix_beta2_y <- matrix(rep(beta2_y,n_subjects),byrow=TRUE,nrow=n_subjects); 
-  y_short_term_rho <- 0.8;
   if (simulate_binary) {
     eta_y <- matrix_beta0_y + matrix_beta1_y*x1 + matrix_beta2_y*x2;
-    mu_y <- plogis(eta_y);
-    stopifnot((min(mu_y)<.75 | max(mu_y)>.25));
+    mu_y <- plogis(eta_y); 
     y <- apply(mu_y,MARGIN=c(1,2),FUN=rbinom,n=1,size=1);
   } else {
     mu_y <- matrix_beta0_y + matrix_beta1_y*x1 + matrix_beta2_y*x2;
@@ -67,7 +96,7 @@ simulate_tvem_example <- function(
         sqrt(1-y_short_term_rho^2)*rnorm(n_subjects);
     }
     y <- mu_y + sigma_y * y_error_term_AR1;  
-    y <- round(y,3);
+    y <- round(y,round_digits);
   } 
   for (this_subject in 1:n_subjects) {
     unobserved_for_this_subject <- sample(1:ncol(y),size=n_obs_possible - n_obs_per_subject[this_subject]);
